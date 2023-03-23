bfdbd594400e27cbc16fbe9ed147addd6995d1ee
beginoffile <cursos.mapatupisaro@gmail.com>
2023-03-23 14:17:37 -0600

    begin functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 import './styles.css';
+
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.querySelector('#play-btn');
+const volumeIcon = document.querySelector('#volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
+
+function showPlayIcon(){
+    playBtn.classList.replace('fa-pause','fa-play');
+    playBtn.setAttribute('title','Play');
+}
+
+function togglePlay(){
+    if (video.paused){
+        video.play();
+        playBtn.classList.replace('fa-play','fa-pause');
+        playBtn.setAttribute('title','Paused');
+    }else{
+        video.pause();
+        showPlayIcon();        
+    }
+}
+
+video.addEventListener('ended',showPlayIcon);
+
+
+function displayTime(time){
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9? seconds : `0${seconds}`;
+    minutes = minutes > 9? minutes : `0${minutes}`;
+    return `${minutes}:${seconds}`;
+}
+
+function updateProgress(){
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
+}
+
+
+playBtn.addEventListener('click',togglePlay);
+video.addEventListener('click',togglePlay);
+video.addEventListener('timeupdate',updateProgress);
+video.addEventListener('canplay',updateProgress);
