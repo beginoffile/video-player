fb346075c072c884f73b7a92b6cef507ac2fbd4f
beginoffile <cursos.mapatupisaro@gmail.com>
2023-03-24 11:19:20 -0600

    add control volume

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const video = document.querySelector('video');
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
 const playBtn = document.querySelector('#play-btn');
+const volumeIconDiv = document.querySelector('.volume-icon');
 const volumeIcon = document.querySelector('#volume-icon');
 const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
@@ -50,9 +51,65 @@ function setProgress(e){
     video.currentTime = newTime * video.duration;
 }
 
+let lastVolume = 1;
+
+function setIcon(volume, isMuted){
+console.log('seteando',volume)    
+    volumeIcon.className = '';
+    if (volume > 0.7){
+        volumeIcon.classList.add('fas','fa-volume-up');
+    }else if (volume < 0.7 && volume > 0){
+        volumeIcon.classList.add('fas','fa-volume-down');
+    }else if (volume ===0){
+        // volumeIconDiv.classList.add('incorrecto');
+        volumeIcon.classList.add('fas','fa-volume-off', 'muted');
+
+        if (isMuted){
+            volumeIcon.classList.add('fa-volume-mute');
+        }
+        
+        
+    }
+}
+
+function changeVolume(e){
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    if (volume < 0.1){
+        volume = 0;
+    }
+    if (volume > 0.9){
+        volume = 1;
+    }
+    volumeBar.style.width = `${volume*100}%`;
+    video.volume = volume;
+    console.log(volume);
+    setIcon(volume,0);
+    lastVolume = volume;
+}
+
+// mute/ unmute
+function toggleMute(){
+    let isMuted = 0;
+    if (video.volume){
+        lastVolume              = video.volume;
+        video.volume            = 0;
+        volumeBar.style.width   = 0;            
+        isMuted                 = 1;
+        
+    } else {
+        video.volume = lastVolume;
+        volumeBar.style.width = `${lastVolume * 100}%`;
+        
+    }
+    
+    setIcon(video.volume,isMuted);
+}
+
 
 playBtn.addEventListener('click',togglePlay);
 video.addEventListener('click',togglePlay);
 video.addEventListener('timeupdate',updateProgress);
 video.addEventListener('canplay',updateProgress);
 progressRange.addEventListener('click',setProgress);
+volumeRange.addEventListener('click',changeVolume);
+volumeIcon.addEventListener('click',toggleMute);
