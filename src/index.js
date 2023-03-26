15593b880146b7cd68cd17f98a0c60b3cae3b09b
beginoffile <cursos.mapatupisaro@gmail.com>
2023-03-26 12:41:54 -0600

    finish project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
 
+const player = document.querySelector('.player');
 const video = document.querySelector('video');
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
@@ -107,8 +108,47 @@ function toggleMute(){
 }
 
 function changeSpeed(){
-    console.log('video playback rate',video.playbackRate);
-    console.log('selected',speed.value);
+    video.playbackRate = speed.value;
+}
+
+
+
+/* View in fullscreen */
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+  video.classList.add('video-fullscreen');
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+  video.classList.remove('video-fullscreen');
+}
+
+let fullscreen = false;
+
+function toggleFullscreen(){
+    // if (!fullscreen){
+    //     openFullscreen(player);
+    // }else{
+    //     closeFullscreen();
+    // }
+
+    !fullscreen ?  openFullscreen(player):closeFullscreen();
+
+    fullscreen = !fullscreen;
 }
 
 
@@ -120,3 +160,4 @@ progressRange.addEventListener('click',setProgress);
 volumeRange.addEventListener('click',changeVolume);
 volumeIcon.addEventListener('click',toggleMute);
 speed.addEventListener('change',changeSpeed);
+fullscreenBtn.addEventListener('click',toggleFullscreen)
